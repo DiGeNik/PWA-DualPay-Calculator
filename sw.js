c20ce3ca9289e196aac63d207c9c29087e864954
DiGeNik <128228683+DiGeNik@users.noreply.github.com>
2026-01-09 03:03:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,19 @@
-const CACHE_NAME = 'dualpay-v1';
+const CACHE_NAME = 'dualpay-v2';
 const assets = [
-  './',
   './index.html',
   './manifest.json',
   'https://cdn.tailwindcss.com'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
